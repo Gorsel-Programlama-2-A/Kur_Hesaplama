cfc3903fa30371061d089a26b7c9229f55a65a9e
Zeynep Şahin <149469550+zayzayla@users.noreply.github.com>
2023-11-13 20:53:28 +0300

    Update 224526051_zeynepsahin_kurhesap.py

--- 224526051_zeynepsahin_kurhesap.py
+++ 224526051_zeynepsahin_kurhesap.py
@@ -1,52 +1,90 @@
+import tkinter as tk
 from tkinter import *
-from tkinter import ttk
+import tkinter.messagebox
+from forex_python.converter import CurrencyRates
 
-usd_kuru = 28
-euro_kuru = 30
+def kur_cevir():
+    c = CurrencyRates()
 
-def kur_hesapla():
-    try:
-        deger = float(e_miktar_giris.get())
-        secili_kur = cmb_kursec.get()
+    kur1 = variable1.get()
+    kur2 = variable2.get()
 
-        if secili_kur == "USD":
-            sonuc = deger / usd_kuru
-        elif secili_kur == "EURO":
-            sonuc = deger / euro_kuru
-        else:
-            sonuc = "Geçersiz Kur"
+    if (kur1Alan.get() == ""):
+        tkinter.messagebox.showinfo("Hata !!", "Tutar Girilmedi.\n Lütfen geçerli bir tutar girin.")
 
-        lbl_hesapsonuc.config(text=f"Sonuç: {sonuc:.3f}")
-    except ValueError:
-        lbl_hesapsonuc.config(text="Geçersiz Giriş")
+    elif (kur1 == "kur" or kur2 == "kur"):
+        tkinter.messagebox.showinfo("Hata !!", "Para Birimi Seçilmedi.")
 
-def input_temizle():
-    e_miktar_giris.delete(0, END)
-    lbl_hesapsonuc.config(text=".....")
+    else:
+        yeni_fark = c.convert(kur1, kur2, float(kur1Alan.get()))
+        yeni_fiyat = float("{:.4f}".format(yeni_fark))
+        kur2Alan.insert(0, str(yeni_fiyat))
 
-anaform = Tk()
-anaform.title("Kur Hesaplama")
-anaform.geometry("500x600")
-anaform.configure(bg="pink")
+def temizle():
+    kur1Alan.delete(0, tk.END)
+    kur2Alan.delete(0, tk.END)
 
-lbl_tlmiktar = Label(anaform, text="TL Miktarı")
-cmb_kursec = ttk.Combobox(anaform, values=["USD", "EURO"])
-lbl_kurmiktar = Label(anaform, text="Seçilen Kur")
-btn_bos = Button(anaform, text="Temizle", command=input_temizle)
-btn_hesapla = ttk.Button(anaform, text="Hesapla", command=kur_hesapla)
-lbl_sonuc = Label(anaform, text="Sonuç")
-lbl_hesapsonuc = Label(anaform, text="........")
-e_miktar_giris = ttk.Entry(anaform)
+anapano = tk.Tk()
 
-lbl_tlmiktar.grid(row=0, column=0, padx=5, pady=5)
-e_miktar_giris.grid(row=0, column=1, padx=5, pady=5)
-lbl_kurmiktar.grid(row=1, column=0, padx=5)
-cmb_kursec.grid(row=1, column=1, padx=5, pady=5)
-lbl_sonuc.grid(row=2, column=0, padx=5, pady=5)
-lbl_hesapsonuc.grid(row=2, column=1, padx=5, pady=5)
-btn_bos.grid(row=4, column=0, padx=6, pady=6)
-btn_hesapla.grid(row=4, column=1, padx=6, pady=6)
+anapano.title("Kur Hesaplama")
 
-anaform.mainloop()
+baslikAlan = Frame(anapano, bg='#264729', pady=2, width=1850, height=100, relief="ridge")
+baslikAlan.grid(row=0, column=0)
 
+baslik = tk.Label(baslikAlan, font=('arial', 19, 'bold'), text='Kur Hesaplama', bg='#264729', fg='black')
+baslik.grid(row=1, column=0, sticky=W)
 
+variable1 = tk.StringVar(anapano)
+variable2 = tk.StringVar(anapano)
+
+variable1.set("kur")
+variable2.set("kur")
+
+kur_listesi = ["USD", "EUR"]
+
+anapano.configure(background='#264729')
+anapano.geometry("400x400")
+
+Label_1 = Label(anapano, font=('arial', 27, 'bold'), text="", padx=2, pady=2, bg="#264729", fg="black")
+Label_1.grid(row=1, column=0, sticky=W)
+
+label1 = tk.Label(anapano, font=('arial', 15, 'bold'), text="\t Tutar : ", bg="#264729", fg="black")
+label1.grid(row=2, column=0, sticky=W)
+
+label1 = tk.Label(anapano, font=('arial', 15, 'bold'), text="\t 1. Kur : ", bg="#264729", fg="black")
+label1.grid(row=3, column=0, sticky=W)
+
+label1 = tk.Label(anapano, font=('arial', 15, 'bold'), text="\t 2. Kur : ", bg="#264729", fg="black")
+label1.grid(row=4, column=0, sticky=W)
+
+label1 = tk.Label(anapano, font=('arial', 15, 'bold'), text="\t Hesaplanmış Kur : ", bg="#264729", fg="black")
+label1.grid(row=8, column=0, sticky=W)
+
+Label_1 = Label(anapano, font=('arial', 7, 'bold'), text="", padx=2, pady=2, bg="#264729", fg="black")
+Label_1.grid(row=5, column=0, sticky=W)
+
+Label_1 = Label(anapano, font=('arial', 7, 'bold'), text="", padx=2, pady=2, bg="#264729", fg="black")
+Label_1.grid(row=7, column=0, sticky=W)
+
+kur1_secenek = tk.OptionMenu(anapano, variable1, *kur_listesi)
+kur2_secenek = tk.OptionMenu(anapano, variable2, *kur_listesi)
+
+kur1_secenek.grid(row=3, column=0, ipadx=45, sticky=E)
+kur2_secenek.grid(row=4, column=0, ipadx=45, sticky=E)
+
+kur1Alan = tk.Entry(anapano)
+kur1Alan.grid(row=2, column=0, ipadx=28, sticky=E)
+
+kur2Alan = tk.Entry(anapano)
+kur2Alan.grid(row=8, column=0, ipadx=31, sticky=E)
+
+Label_9 = Button(anapano, font=('arial', 15, 'bold'), text=" Hesapla ", padx=2, pady=2, bg="lightblue", fg="white", command=kur_cevir)
+Label_9.grid(row=6, column=0)
+
+Label_1 = Label(anapano, font=('arial', 7, 'bold'), text="", padx=2, pady=2, bg="#264729", fg="black")
+Label_1.grid(row=9, column=0, sticky=W)
+
+Label_9 = Button(anapano, font=('arial', 15, 'bold'), text="Temizle ", padx=2, pady=2, bg="lightblue", fg="white", command=temizle)
+Label_9.grid(row=10, column=0)
+
+anapano.mainloop()
