92ffa876956d31fd9b62137a5e7439844b769b70
Murat Can Kardaş <126358041+mucaka@users.noreply.github.com>
2023-11-13 22:04:39 +0300

    Update A_224526037_MuratCanKardas.py

--- A_224526037_MuratCanKardas.py
+++ A_224526037_MuratCanKardas.py
@@ -1,13 +1,15 @@
 from tkinter import *
 from tkinter import ttk
 
+usd =  28
+euroKuru = 30
 
 anaform = Tk()
 
 anaform.title("Kur Hesapla")
 anaform.geometry("400x600")
 
-lbl_tlmiktar = Label(anaform, text="Tl Miktarını Yazınız: ")
+lbl_tlmiktar = Label(anaform, text="TL Miktarını Yazınız: ")
 lbl_kurtur = ttk.Label(anaform, text="Kur Türünü Seçiniz: ")
 lbl_sonuc = ttk.Label(anaform, text="Sonuç: ")
 lbl_hesapsonuc = ttk.Label(anaform, text="...........")
@@ -15,15 +17,6 @@ cmb_kursec = ttk.Combobox(anaform, values=["USD", "EURO"])
 
 btn_1 = Button(anaform, text="Boş")
 btn_hesapla = ttk.Button(anaform, text="Hesapla")
-
-
-
-"""
-lbl_tlmiktar.pack(expand=NO, fill = BOTH, padx=40, pady=20)
-cmb_kursec.pack(expand=NO, fill = BOTH, padx=40, pady=20)
-btn_1.pack(expand=NO, fill = X, padx=40, pady=20)
-btn_hesapla.pack(expand=NO, side = BOTTOM)
-"""
 
 e_miktar_giris = ttk.Entry(anaform)
 lbl_tlmiktar.grid(row=0, column=0)
@@ -34,6 +27,24 @@ btn_1.grid(row=3, column=0, padx=20, pady=20)
 btn_hesapla.grid(row=3, column=1, padx=20, pady=20)
 lbl_sonuc.grid(row=2, column= 0)
 lbl_hesapsonuc.grid(row=2, column= 1)
+
+
+def hesapla():
+    miktar = int(e_miktar_giris.get())
+    kur = cmb_kursec.get()
+
+    if kur == "USD":
+        sonuc = miktar / usd
+    elif kur == "EURO":
+        sonuc = miktar / euroKuru
+
+    #yuvarlama
+    sonuc = round(sonuc, 2)
+
+    lbl_hesapsonuc.config(text=str(sonuc))
+
+
+btn_hesapla.config(command=hesapla)
 
 
 anaform.mainloop()
